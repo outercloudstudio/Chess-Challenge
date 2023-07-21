f0fbf4e1b990eedb49a2c775c05431820c970a37
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-07-21 12:49:01 -0500

    Bot 1: Stupid 1 Move Evaluation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,63 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
-    public Move Think(Board board, Timer timer)
+  int Evaluate(Board board, Move move)
+  {
+    board.MakeMove(move);
+    bool isMate = board.IsInCheckmate();
+    bool isCheck = board.IsInCheck();
+    board.UndoMove(move);
+
+    if (isMate) return 1000000;
+
+    if (isCheck) return 1000;
+
+    if (move.IsCapture) return 500;
+
+    if (move.IsCastles) return 200;
+
+    return 1;
+  }
+
+  int ChooseMove(int[] evaluations)
+  {
+    int evaluationSum = 0;
+
+    foreach (int evaluation in evaluations)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+      evaluationSum += evaluation;
     }
+
+    int target = (int)new System.Random().NextInt64(0, evaluationSum);
+
+    int indexSum = 0;
+
+    for (int index = 0; index < evaluations.Length; index++)
+    {
+      indexSum += evaluations[index];
+
+      if (indexSum > target) return index;
+    }
+
+    return evaluations.Length - 1;
+  }
+
+  public Move Think(Board board, Timer timer)
+  {
+    Move[] moves = board.GetLegalMoves();
+    int[] evaluations = new int[moves.Length];
+
+    for (int moveIndex = 0; moveIndex < moves.Length; moveIndex++)
+    {
+      evaluations[moveIndex] = Evaluate(board, moves[moveIndex]);
+    }
+
+    int chosenMoveIndex = ChooseMove(evaluations);
+
+    Console.WriteLine(chosenMoveIndex);
+
+    return moves[chosenMoveIndex];
+  }
 }
