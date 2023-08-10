c4c250a0595f450bdd28631902e575f0d8591c17
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-08-10 00:09:34 -0500

    feat: piece square tables, but bugged

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -17,7 +17,34 @@ public class MyBot : IChessBot
 
   int nodesSearched = 0;
 
-  int[] pieceValues = new int[] { 0, 100, 300, 300, 500, 900, 10000 };
+  int[] phase_weight = { 0, 1, 1, 2, 4, 0 };
+  int[] pieceValues = { 82, 337, 365, 477, 1025, 20000, 94, 281, 297, 512, 936, 20000 };
+
+  decimal[] packedPieceTables = {
+    63746705523041458768562654720m, 71818693703096985528394040064m, 75532537544690978830456252672m, 75536154932036771593352371712m, 76774085526445040292133284352m, 3110608541636285947269332480m, 936945638387574698250991104m, 75531285965747665584902616832m,
+    77047302762000299964198997571m, 3730792265775293618620982364m, 3121489077029470166123295018m, 3747712412930601838683035969m, 3763381335243474116535455791m, 8067176012614548496052660822m, 4977175895537975520060507415m, 2475894077091727551177487608m,
+    2458978764687427073924784380m, 3718684080556872886692423941m, 4959037324412353051075877138m, 3135972447545098299460234261m, 4371494653131335197311645996m, 9624249097030609585804826662m, 9301461106541282841985626641m, 2793818196182115168911564530m,
+    77683174186957799541255830262m, 4660418590176711545920359433m, 4971145620211324499469864196m, 5608211711321183125202150414m, 5617883191736004891949734160m, 7150801075091790966455611144m, 5619082524459738931006868492m, 649197923531967450704711664m,
+    75809334407291469990832437230m, 78322691297526401047122740223m, 4348529951871323093202439165m, 4990460191572192980035045640m, 5597312470813537077508379404m, 4980755617409140165251173636m, 1890741055734852330174483975m, 76772801025035254361275759599m,
+    75502243563200070682362835182m, 78896921543467230670583692029m, 2489164206166677455700101373m, 4338830174078735659125311481m, 4960199192571758553533648130m, 3420013420025511569771334658m, 1557077491473974933188251927m, 77376040767919248347203368440m,
+    73949978050619586491881614568m, 77043619187199676893167803647m, 1212557245150259869494540530m, 3081561358716686153294085872m, 3392217589357453836837847030m, 1219782446916489227407330320m, 78580145051212187267589731866m, 75798434925965430405537592305m,
+    68369566912511282590874449920m, 72396532057599326246617936384m, 75186737388538008131054524416m, 77027917484951889231108827392m, 73655004947793353634062267392m, 76417372019396591550492896512m, 74568981255592060493492515584m, 70529879645288096380279255040m,
+  };
+
+  int[][] pieceTables;
+
+  public MyBot()
+  {
+    pieceTables = new int[64][];
+    pieceTables = packedPieceTables.Select(packedTable =>
+    {
+      int pieceType = 0;
+      return decimal.GetBits(packedTable).Take(3)
+        .SelectMany(c => BitConverter.GetBytes(c)
+          .Select((byte square) => (int)((sbyte)square * 1.461) + pieceValues[pieceType++]))
+        .ToArray();
+    }).ToArray();
+  }
 
   bool hasTime => _timer.MillisecondsElapsedThisTurn < _timer.MillisecondsRemaining / 60;
 
@@ -25,22 +52,38 @@ public class MyBot : IChessBot
   {
     if (move == bestHashMove) return 99999999;
 
-    if (move.IsCapture) return pieceValues[(int)move.CapturePieceType] - pieceValues[(int)move.MovePieceType] / 100;
+    if (move.IsCapture) return pieceValues[(int)move.CapturePieceType - 1] - pieceValues[(int)move.MovePieceType - 1] / 100;
 
     return _historyTable[_board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType - 1, move.TargetSquare.Index];
   }
 
   int Evaluate()
   {
-    int materialEvaluation = 0;
+    int middleGame = 0, endGame = 0, phase = 0;
 
-    for (int typeIndex = 1; typeIndex < 7; typeIndex++)
+    foreach (bool white in new[] { true, false })
     {
-      materialEvaluation += _board.GetPieceList((PieceType)typeIndex, _board.IsWhiteToMove).Count * pieceValues[typeIndex];
-      materialEvaluation -= _board.GetPieceList((PieceType)typeIndex, !_board.IsWhiteToMove).Count * pieceValues[typeIndex];
+      for (int piece = -1; ++piece < 6;)
+      {
+        ulong bitBoard = _board.GetPieceBitboard((PieceType)(piece + 1), white);
+
+        while (bitBoard != 0)
+        {
+          int sq = BitboardHelper.ClearAndGetIndexOfLSB(ref bitBoard) ^ (white ? 56 : 0);
+
+          middleGame += pieceTables[sq][piece];
+          endGame += pieceTables[sq][piece + 6];
+
+          phase += phase_weight[piece];
+        }
+      }
+      middleGame = -middleGame;
+      endGame = -endGame;
     }
 
-    return materialEvaluation;
+    phase = Math.Min(phase, 24);
+
+    return (middleGame * phase + endGame * (24 - phase)) / 24 * (_board.IsWhiteToMove ? 1 : -1);
   }
 
   record struct OrderedMove(Move Move, int Interest);
@@ -71,16 +114,16 @@ public class MyBot : IChessBot
       bestMove = transpositionEntry.BestMove;
     }
 
-    Move[] moves = _board.GetLegalMoves(qSearch);
-
     int max = -99999999 + ply;
-
-    if (qSearch) max = Evaluate();
 
     if (depth <= 0 && !qSearch) return Evaluate();
     else
     {
+      Move[] moves = _board.GetLegalMoves(qSearch);
+
       if (_board.IsRepeatedPosition() || _board.IsFiftyMoveDraw()) return _white == _board.IsWhiteToMove ? -5 : 5;
+
+      if (qSearch && moves.Length == 0) return Evaluate();
 
       var orderedMoves = moves.Select(move => new OrderedMove(move, Interest(move, bestMove))).OrderByDescending(orderedMove => orderedMove.Interest);
 
@@ -92,7 +135,7 @@ public class MyBot : IChessBot
 
         _board.MakeMove(move);
 
-        int score = -AlphaBetaWM(-upperBound, -lowerBound, ply + 1, depth - 1, depth == 1 && move.IsCapture);
+        int score = -AlphaBetaWM(-upperBound, -lowerBound, ply + 1, depth - 1, depth <= 1 && move.IsCapture);
 
         _board.UndoMove(move);
 
