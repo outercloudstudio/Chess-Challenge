607b51cbceb48d5145c0f93b1f98add1512229dc
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-08-12 00:53:00 -0500

    feat: revert to simplified bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Linq;
 using ChessChallenge.API;
+using Frederox.AlphaBeta;
 
 public class MyBot : IChessBot
 {
-  record struct TranspositionEntry(ulong Hash, int Depth, int LowerBound, int UpperBound, Move BestMove);
+  record struct TranspositionEntry(ulong Hash, int Depth, int Score, int Bound, Move BestMove);
   TranspositionEntry[] _transpositionTable = new TranspositionEntry[400000];
-
-  int[,,] _historyTable;
 
   Board _board;
   Move _bestMove;
@@ -47,13 +46,11 @@ public class MyBot : IChessBot
 
   bool hasTime => _timer.MillisecondsElapsedThisTurn < _timer.MillisecondsRemaining / 60;
 
-  int Interest(Move move, Move bestHashMove)
+  int Interest(Move move)
   {
-    if (move == bestHashMove) return 99999999;
+    if (move.IsCapture) return 100 * pieceValues[(int)move.CapturePieceType - 1] - pieceValues[(int)move.MovePieceType - 1];
 
-    if (move.IsCapture) return pieceValues[(int)move.CapturePieceType - 1] - pieceValues[(int)move.MovePieceType - 1] / 100;
-
-    return _historyTable[_board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType - 1, move.TargetSquare.Index];
+    return 0;
   }
 
   int Evaluate()
@@ -87,137 +84,67 @@ public class MyBot : IChessBot
 
   record struct OrderedMove(Move Move, int Interest);
 
-  int AlphaBetaWM(int lowerBound, int upperBound, int ply, int depth, bool qSearch)
+  int Search(int lowerBound, int upperBound, int ply, int depth)
   {
     nodesSearched++;
 
+    if (depth <= 0) return Evaluate();
+
+    Move[] moves = _board.GetLegalMoves();
+
+    var orderedMoves = moves.Select(move => new OrderedMove(move, Interest(move))).OrderByDescending(orderedMove => orderedMove.Interest);
+
     Move bestMove = Move.NullMove;
 
-    ulong hash = _board.ZobristKey;
-    ulong key = hash % 100000L;
-    TranspositionEntry transpositionEntry = _transpositionTable[key];
-
-    if (!qSearch && transpositionEntry.Depth > 0 && transpositionEntry.Hash == hash)
+    foreach (OrderedMove orderedMove in orderedMoves)
     {
-      if (transpositionEntry.Depth >= depth)
+      if (!_initialSearch && !hasTime) break;
+
+      Move move = orderedMove.Move;
+
+      _board.MakeMove(move);
+
+      int score = -Search(-upperBound, -lowerBound, ply + 1, depth - 1);
+
+      _board.UndoMove(move);
+
+      if (_bestMove == Move.NullMove) _bestMove = move;
+
+      if (score >= upperBound)
       {
-        if (transpositionEntry.LowerBound >= upperBound) return transpositionEntry.LowerBound;
-        if (transpositionEntry.UpperBound <= lowerBound) return transpositionEntry.UpperBound;
+        bestMove = move;
 
-        lowerBound = Math.Max(lowerBound, transpositionEntry.LowerBound);
-        upperBound = Math.Min(upperBound, transpositionEntry.UpperBound);
+        lowerBound = upperBound;
 
-        if (ply == 0) _bestMove = transpositionEntry.BestMove;
+        break;
       }
 
-      bestMove = transpositionEntry.BestMove;
-    }
-
-    int max = -99999999 + ply;
-
-    if (depth <= 0 && !qSearch) return Evaluate();
-    else
-    {
-      Move[] moves = _board.GetLegalMoves(qSearch);
-
-      // if (_board.IsRepeatedPosition() || _board.IsFiftyMoveDraw()) return _white == _board.IsWhiteToMove ? -5 : 5;
-
-      if (qSearch)
+      if (score > lowerBound)
       {
-        if (moves.Length == 0) return Evaluate();
+        bestMove = move;
 
-        int standingScore = Evaluate();
-
-        if (standingScore >= upperBound) return standingScore;
-
-        if (standingScore > lowerBound) lowerBound = standingScore;
-      }
-
-      var orderedMoves = moves.Select(move => new OrderedMove(move, Interest(move, bestMove))).OrderByDescending(orderedMove => orderedMove.Interest);
-
-      foreach (OrderedMove orderedMove in orderedMoves)
-      {
-        if (!_initialSearch && !hasTime) break;
-
-        Move move = orderedMove.Move;
-
-        _board.MakeMove(move);
-
-        int score = -AlphaBetaWM(-upperBound, -lowerBound, ply + 1, depth - 1, depth <= 1 && move.IsCapture);
-
-        _board.UndoMove(move);
-
-        if (score >= upperBound)
-        {
-          bestMove = move;
-
-          max = score;
-
-          upperBound = score;
-
-          if (!move.IsCapture)
-            _historyTable[_board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType - 1, move.TargetSquare.Index] += depth * depth;
-
-          break;
-        }
-
-        if (score > max)
-        {
-          max = score;
-
-          bestMove = move;
-
-          if (score > lowerBound) lowerBound = score;
-        }
+        lowerBound = score;
       }
     }
-
-    if (!qSearch && depth >= transpositionEntry.Depth) _transpositionTable[key] = new TranspositionEntry(hash, depth, lowerBound, upperBound, bestMove);
 
     if (ply == 0) _bestMove = bestMove;
 
-    return max;
+    return lowerBound;
   }
-
-  int MTDF(int initialGuess, int depth)
-  {
-    int max = initialGuess;
-
-    int upperBound = 99999999;
-    int lowerBound = -99999999;
-
-    while (lowerBound < upperBound)
-    {
-      int beta = max;
-      if (max == lowerBound) beta++;
-
-      max = AlphaBetaWM(beta - 1, beta, 0, depth, false);
-
-      if (max < beta) upperBound = max;
-      else lowerBound = max;
-    }
-
-    return max;
-  }
-
-  int bestMoveGuess = 0;
 
   public Move Think(Board board, Timer timer)
   {
     _board = board;
     _timer = timer;
 
-    _historyTable = new int[2, 6, 64];
-
     int depth = 1;
+    _initialSearch = true;
 
     while (_initialSearch || hasTime)
     {
-      _initialSearch = depth == 1;
-
       Move lastBestMove = _bestMove;
 
-      bestMoveGuess = MTDF(bestMoveGuess, depth);
+      Search(-99999999, 99999999, 0, depth);
 
       if (!_initialSearch && !hasTime)
       {
@@ -229,6 +156,8 @@ public class MyBot : IChessBot
       Console.WriteLine($"Searched to depth {depth} in {timer.MillisecondsElapsedThisTurn}ms"); // #DEBUG
 
       depth++;
+
+      _initialSearch = false;
     }
 
     depth--;
