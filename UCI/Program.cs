0ac7cb627e2d5cdca591c09a00a6eaeca506a700
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-08-13 01:46:34 -0500

    feat: uci

---
+++ UCI/Program.cs
@@ -0,0 +1,20 @@
+﻿using ChessChallenge.Application;
+
+namespace Chess_Challenge.Cli
+{
+  internal class Program
+  {
+    static (int, int) GetTokenCount()
+    {
+      string path = Path.Combine(Directory.GetCurrentDirectory(), "src", "My Bot", "MyBot.cs");
+      string txt = File.ReadAllText(path);
+      return TokenCounter.CountTokens(txt);
+    }
+
+    static void Main(string[] args)
+    {
+      var uci = new Uci(args);
+      uci.Run();
+    }
+  }
+}
