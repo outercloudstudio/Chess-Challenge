07530af07b2fc58e7a65f916fc8e2cc5d3a15546
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-09-21 21:03:47 -0500

    token optimizations

--- Chess-Challenge/src/My Bot/MyBotEvil.cs
+++ Chess-Challenge/src/My Bot/MyBotEvil.cs
@@ -1,235 +1,227 @@
 ﻿using System;
-using System.Linq;
 using ChessChallenge.API;
 
 public class MyBotEvil : IChessBot
 {
-  /*
-    TODO:
-    - Experiment with MTDf and PV again
-    - Aspiration windows
-    - Bad move pruning experiment
-  */
-
-  // Bounds:
-  // 0 = Exact
-  // 1 = Lower, Never found a move greater than alpha
-  // 2 = Upper, found a move better than oponent reposonses
-  record struct TranspositionEntry(ulong Hash, int Score, int Bound, Move BestMove, int Depth = -1);
-  TranspositionEntry[] _transpositionTable = new TranspositionEntry[400000];
-
-  int[,,] _historyTable;
-
-  Board _board;
-  Move _bestMove;
-  int _evaluation = 0;
-  Timer _timer;
-  bool _initialSearch;
-
-  int nodesSearched = 0;
-
-  int[] phase_weight = { 0, 1, 1, 2, 4, 0 };
-  int[] pieceValues = { 82, 337, 365, 477, 1025, 20000, 94, 281, 297, 512, 936, 20000 };
-
-  decimal[] packedPieceTables = {
-    63746705523041458768562654720m, 71818693703096985528394040064m, 75532537544690978830456252672m, 75536154932036771593352371712m, 76774085526445040292133284352m, 3110608541636285947269332480m, 936945638387574698250991104m, 75531285965747665584902616832m,
-    77047302762000299964198997571m, 3730792265775293618620982364m, 3121489077029470166123295018m, 3747712412930601838683035969m, 3763381335243474116535455791m, 8067176012614548496052660822m, 4977175895537975520060507415m, 2475894077091727551177487608m,
-    2458978764687427073924784380m, 3718684080556872886692423941m, 4959037324412353051075877138m, 3135972447545098299460234261m, 4371494653131335197311645996m, 9624249097030609585804826662m, 9301461106541282841985626641m, 2793818196182115168911564530m,
-    77683174186957799541255830262m, 4660418590176711545920359433m, 4971145620211324499469864196m, 5608211711321183125202150414m, 5617883191736004891949734160m, 7150801075091790966455611144m, 5619082524459738931006868492m, 649197923531967450704711664m,
-    75809334407291469990832437230m, 78322691297526401047122740223m, 4348529951871323093202439165m, 4990460191572192980035045640m, 5597312470813537077508379404m, 4980755617409140165251173636m, 1890741055734852330174483975m, 76772801025035254361275759599m,
-    75502243563200070682362835182m, 78896921543467230670583692029m, 2489164206166677455700101373m, 4338830174078735659125311481m, 4960199192571758553533648130m, 3420013420025511569771334658m, 1557077491473974933188251927m, 77376040767919248347203368440m,
-    73949978050619586491881614568m, 77043619187199676893167803647m, 1212557245150259869494540530m, 3081561358716686153294085872m, 3392217589357453836837847030m, 1219782446916489227407330320m, 78580145051212187267589731866m, 75798434925965430405537592305m,
-    68369566912511282590874449920m, 72396532057599326246617936384m, 75186737388538008131054524416m, 77027917484951889231108827392m, 73655004947793353634062267392m, 76417372019396591550492896512m, 74568981255592060493492515584m, 70529879645288096380279255040m,
-  };
-
-  int[][] pieceTables;
+  float[] _parameters = new float[1418];
 
   public MyBotEvil()
   {
-    pieceTables = new int[64][];
-    pieceTables = packedPieceTables.Select(packedTable =>
+    for (int parameter = 0; parameter < 1418; parameter++)
     {
-      int pieceType = 0;
-      return decimal.GetBits(packedTable).Take(3)
-        .SelectMany(c => BitConverter.GetBytes(c)
-          .Select((byte square) => (int)((sbyte)square * 1.461) + pieceValues[pieceType++]))
-        .ToArray();
-    }).ToArray();
+      var ints = decimal.GetBits(_compressedParameters[parameter / 16]);
+      int bits = parameter % 16 * 6;
+      int bitsOffset = bits % 32;
+      int intIndex = bits / 32;
+
+      int quantized = ints[intIndex] >> bitsOffset & 0b111111;
+      if (bitsOffset > 27) quantized |= ints[intIndex + 1] << 32 - bitsOffset & 0b111111;
+
+      _parameters[parameter] = MathF.Pow(quantized / 64f - 0.5f, 3) * 6f;
+    }
   }
 
-  bool hasTime => _timer.MillisecondsElapsedThisTurn < _timer.MillisecondsRemaining / 60;
+  int parameterOffset = 0;
 
-  int Interest(Move move, Move bestMove)
+  float[] _layerInput = new float[54];
+  float[] _layerOutput = new float[16];
+  float[] _evaluationTensor = new float[37];
+  float[] _sightTensor = new float[54];
+  float[] _emptyTensor = new float[54];
+
+  void Layer(int previousLayerSize, int layerSize)
   {
-    if (move == bestMove) return 999999999;
+    Array.Copy(_emptyTensor, _layerOutput, 16);
 
-    if (move.IsCapture) return 100 * pieceValues[(int)move.CapturePieceType - 1] - pieceValues[(int)move.MovePieceType - 1];
-
-    return _historyTable[_board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType - 1, move.TargetSquare.Index];
-  }
-
-  int Evaluate()
-  {
-    int middleGame = 0, endGame = 0, phase = 0;
-
-    foreach (bool white in new[] { true, false })
+    for (int nodeIndex = 0; nodeIndex < layerSize; nodeIndex++)
     {
-      for (int piece = -1; ++piece < 6;)
+      for (int weightIndex = 0; weightIndex < previousLayerSize; weightIndex++)
       {
-        ulong bitBoard = _board.GetPieceBitboard((PieceType)(piece + 1), white);
+        _layerOutput[nodeIndex] += _layerInput[weightIndex] * _parameters[parameterOffset + nodeIndex * previousLayerSize + weightIndex];
+      }
 
-        while (bitBoard != 0)
+      _layerOutput[nodeIndex] = MathF.Max(MathF.Min(_layerOutput[nodeIndex] + _parameters[parameterOffset + layerSize * previousLayerSize + nodeIndex], 1), -1);
+    }
+
+    parameterOffset += layerSize * previousLayerSize + layerSize;
+
+    Array.Copy(_layerOutput, _layerInput, layerSize);
+  }
+
+  int[] pieceValues = new int[] { 0, 1, 3, 3, 5, 9, 1000 };
+
+  float Inference()
+  {
+    if (_board.IsInCheckmate()) return -100000 * WhiteToMoveFactor;
+
+    int evaluation = 0;
+
+    for (int type = 1; type < 7; type++)
+    {
+      evaluation += _board.GetPieceList((PieceType)type, true).Count * pieceValues[type];
+      evaluation -= _board.GetPieceList((PieceType)type, false).Count * pieceValues[type];
+    }
+
+    for (int x = 0; x < 6; x++)
+    {
+      for (int y = 0; y < 6; y++)
+      {
+        Array.Copy(_emptyTensor, _sightTensor, 54);
+
+        for (int kernelX = 0; kernelX < 3; kernelX++)
         {
-          int sq = BitboardHelper.ClearAndGetIndexOfLSB(ref bitBoard) ^ (white ? 56 : 0);
+          for (int kernelY = 0; kernelY < 3; kernelY++)
+          {
+            Piece piece = _board.GetPiece(new Square(x + kernelX, y + kernelY));
 
-          middleGame += pieceTables[sq][piece];
-          endGame += pieceTables[sq][piece + 6];
-
-          phase += phase_weight[piece];
+            if (piece.PieceType != PieceType.None) _sightTensor[kernelX * 18 + kernelY * 6 + (int)piece.PieceType - 1] = piece.IsWhite ? 1 : -1;
+          }
         }
+
+        parameterOffset = 0;
+
+        Array.Copy(_sightTensor, _layerInput, 54);
+
+        Layer(54, 8);
+        Layer(8, 8);
+        Layer(8, 1);
+
+        _evaluationTensor[x * 6 + y] = _layerOutput[0];
       }
-      middleGame = -middleGame;
-      endGame = -endGame;
     }
 
-    phase = Math.Min(phase, 24);
+    _evaluationTensor[36] = WhiteToMoveFactor;
 
-    return (middleGame * phase + endGame * (24 - phase)) / 24 * (_board.IsWhiteToMove ? 1 : -1);
+    Array.Copy(_evaluationTensor, _layerInput, 37);
+    Layer(37, 16);
+    Layer(16, 16);
+    Layer(16, 1);
+
+    return _layerOutput[0] + evaluation;
   }
 
-  record struct OrderedMove(Move Move, int Interest);
+  Board _board;
+  Timer _timer;
+  Move _bestMove;
 
-  int Search(int lowerBound, int upperBound, int ply, int depth, bool isLoud)
+  int _nodes; //#DEBUG
+
+  int[] MoveScores = new int[218];
+
+  int WhiteToMoveFactor => _board.IsWhiteToMove ? 1 : -1;
+
+  // Hash, Move, Score, Depth, Bound
+  (ulong, Move, float, int, int)[] _transpositionTable = new (ulong, Move, float, int, int)[40000];
+
+  float Search(int ply, int depth, float alpha, float beta)
   {
-    nodesSearched++;
+    _nodes++; //#DEBUG
 
-    bool qSearch = depth <= 0 && isLoud;
+    ulong zobristKey = _board.ZobristKey;
+    var (transpositionHash, transpositionMove, transpositionScore, transpositionDepth, transpositionFlag) = _transpositionTable[zobristKey % 40000];
 
-    Move bestMove = Move.NullMove;
+    if (transpositionHash == zobristKey && transpositionDepth >= depth && (
+      transpositionFlag == 1 ||
+      transpositionFlag == 2 && transpositionScore <= alpha ||
+      transpositionFlag == 3 && transpositionScore >= beta)
+    ) return transpositionScore;
 
-    ulong hash = _board.ZobristKey;
-    ulong key = hash % 400000L;
-
-    TranspositionEntry transpositionEntry = _transpositionTable[key];
-
-    // Don't get transposition table if qSearch since we are going to search to an undefined depth
-    if (!qSearch && transpositionEntry.Depth > -1 && transpositionEntry.Hash == hash)
-    {
-      bestMove = transpositionEntry.BestMove;
-
-      if (depth <= transpositionEntry.Depth)
-      {
-        if (transpositionEntry.Bound == 0) return transpositionEntry.Score;
-        if (transpositionEntry.Bound == 1 && transpositionEntry.Score >= upperBound) return transpositionEntry.Score;
-        if (transpositionEntry.Bound == 2 && transpositionEntry.Score <= lowerBound) return transpositionEntry.Score;
-
-        lowerBound = Math.Max(lowerBound, transpositionEntry.Score);
-        upperBound = Math.Min(upperBound, transpositionEntry.Score);
-      }
-    }
-
-    // we can't return cause we need to keep q searching
-    if (!qSearch && depth <= 0) return Evaluate();
+    bool qSearch = depth <= 0;
 
     if (qSearch)
     {
-      int standingEvaluation = Evaluate();
+      alpha = MathF.Max(alpha, Inference() * WhiteToMoveFactor);
 
-      if (standingEvaluation >= upperBound) return standingEvaluation;
-
-      lowerBound = Math.Max(lowerBound, standingEvaluation);
+      if (alpha >= beta) return alpha;
     }
 
-    int originalLowerBound = lowerBound;
+    bool isCheck = _board.IsInCheck();
 
-    Move[] moves = _board.GetLegalMoves(qSearch);
+    Span<Move> moves = stackalloc Move[218];
+    _board.GetLegalMovesNonAlloc(ref moves, qSearch && !isCheck);
 
-    var orderedMoves = moves.Select(move => new OrderedMove(move, Interest(move, bestMove))).OrderByDescending(orderedMove => orderedMove.Interest);
+    if (moves.Length == 0) return Inference() * WhiteToMoveFactor;
 
-    foreach (OrderedMove orderedMove in orderedMoves)
+    int index = 0;
+
+    // Scores are sorted low to high
+    foreach (Move move in moves)
     {
-      if (!_initialSearch && !hasTime) break;
+      MoveScores[index++] = move == transpositionMove ? -1000000 : move.IsCapture
+        ? (int)move.MovePieceType - 100 * (int)move.CapturePieceType
+        : 1000000;
+    }
 
-      Move move = orderedMove.Move;
+    MoveScores.AsSpan(0, moves.Length).Sort(moves);
+
+    Move bestMove = moves[0];
+    int newTranspositionFlag = 1;
+
+    foreach (Move move in moves)
+    {
+      if (outOfTime && ply > 0) return 100000000f;
 
       _board.MakeMove(move);
 
-      int score = -Search(-upperBound, -lowerBound, ply + 1, depth - 1, move.IsCapture);
+      float score = -Search(ply + 1, depth - 1, -beta, -alpha);
 
       _board.UndoMove(move);
 
-      if (bestMove == Move.NullMove) bestMove = move;
-
-      if (score >= upperBound)
+      if (score > alpha)
       {
+        newTranspositionFlag = 0;
+
         bestMove = move;
 
-        lowerBound = score;
+        if (ply == 0) _bestMove = move;
 
-        if (!move.IsCapture && !qSearch)
-          _historyTable[_board.IsWhiteToMove ? 0 : 1, (int)move.MovePieceType - 1, move.TargetSquare.Index] += depth * depth;
+        alpha = score;
 
-        break;
-      }
+        if (score >= beta)
+        {
+          newTranspositionFlag = 2;
 
-      if (score > lowerBound)
-      {
-        bestMove = move;
-
-        lowerBound = score;
+          break;
+        }
       }
     }
 
-    if (ply == 0) _bestMove = bestMove;
+    if (!outOfTime && !qSearch) _transpositionTable[zobristKey % 40000] = (zobristKey, bestMove, alpha, depth, newTranspositionFlag);
 
-    int bound = 0;
-    if (lowerBound <= originalLowerBound) bound = 1;
-    if (lowerBound >= upperBound) bound = 2;
-
-    // Can't save qSearch results cause it's incomplete information
-    if (!qSearch && depth >= transpositionEntry.Depth) transpositionEntry = new TranspositionEntry(hash, lowerBound, bound, bestMove, depth);
-
-    return lowerBound;
+    return alpha;
   }
+
+  bool outOfTime => _timer.MillisecondsElapsedThisTurn >= _timer.MillisecondsRemaining / 30f;
 
   public Move Think(Board board, Timer timer)
   {
-    _historyTable = new int[2, 6, 64];
-
     _board = board;
     _timer = timer;
 
+    _nodes = 0; //#DEBUG
+
     int depth = 1;
-    _initialSearch = true;
 
-    while (_initialSearch || hasTime)
+    Move lastBestMove = Move.NullMove;
+    _bestMove = Move.NullMove;
+
+    while (true)
     {
-      Move lastBestMove = _bestMove;
-      int lastEvaluation = _evaluation;
+      Search(0, depth++, -1000000f, 1000000f);
 
-      int score = Search(-999999999, 999999999, 0, depth, false);
+      if (lastBestMove == Move.NullMove) lastBestMove = _bestMove;
 
-      if (!_initialSearch && !hasTime)
-      {
-        _bestMove = lastBestMove;
-        _evaluation = lastEvaluation;
+      if (outOfTime) break;
 
-        break;
-      }
+      lastBestMove = _bestMove;
 
-      // Console.WriteLine($"Searched to depth {depth} in {timer.MillisecondsElapsedThisTurn}ms"); // #DEBUG
-
-      depth++;
-
-      _initialSearch = false;
-
-      if (score > 99999999 / 2) break;
+      if (depth > 50) break;
     }
 
-    depth--;
+    Console.WriteLine($"Nodes per second {_nodes / (timer.MillisecondsElapsedThisTurn / 1000f + 0.00001f)} Depth: {depth} Seconds {timer.MillisecondsElapsedThisTurn / 1000f}"); //#DEBUG
 
-    // Console.WriteLine($"Searched to depth {depth} in {timer.MillisecondsElapsedThisTurn}ms {nodesSearched / (timer.MillisecondsElapsedThisTurn / (float)1000)} nodes/sec"); // #DEBUG
-
-    return _bestMove;
+    return lastBestMove;
   }
+
+  decimal[] _compressedParameters = new decimal[] { 71643580751471398296591240685M, 57808770151261461419226477691M, 19406506132741603410010081711M, 24482052838937212115927924269M, 35488450030206109575574123883M, 44505961270809876616670397460M, 6386205757416436081818797591M, 25518927372226980421006337218M, 23759813701360984535944704917M, 5148567674429100055487624011M, 65430025144897957756759659907M, 54785293903891415770164608693M, 58621329350839794016438352743M, 59677419043468099767816636657M, 55847011185427554312254960426M, 2712260948636043033985346640M, 9079033211120423414756572096M, 57004819721906195134032396679M, 32501453695806711460947358866M, 7829413217067279613767263334M, 16191685549497778701553119624M, 47317279480554206307608112134M, 8758087394345427328314942091M, 27155457833350119556620767723M, 51658818453113445286514981541M, 69028348657143427241524763433M, 51934205895145868172533681783M, 65207431665110062407286114012M, 16476965894721585970540000915M, 19881998466025449883699163916M, 63988824764756138895881190247M, 36549338497560467735161175413M, 8802884638218255110629284083M, 24030207099822022709986796679M, 8807360347731295831221629391M, 22663927820590848568716981592M, 26366101318163985896470115411M, 52888618345146642650996756934M, 20104505686152296584988134376M, 71662972483713349080852569136M, 57824030357890306811820739374M, 67754147866867210581916188216M, 51030800339494603283610414055M, 63893763633431852281605842083M, 52344139399671199669525982190M, 12586505396258988918759951409M, 22401126729864773123343548940M, 8981512959445864384542040530M, 46591620132819955298537907143M, 25255132075702536855666703318M, 28738200987018938277227709873M, 47472132678762306041403175404M, 7565236942929439305419269290M, 18853874505270817498542445830M, 19163371283611636837956121613M, 12035211327720050287397643090M, 30026316197745443247095341769M, 18766064531419282377772729687M, 23978051483937635908706803256M, 72900912677490329324072131309M, 14463343690081442275149454189M, 20023035597168653870810170503M, 8807124290459376578932286167M, 42540065527031475380029301191M, 58591526226367785436314367123M, 17085341290331636776627969263M, 14600123241326501917554563605M, 18206198034425516280857937383M, 61039374671395954943592143699M, 31729039200191393546303501651M, 26389446575821012298297677272M, 49085833787666536873660695955M, 323068096965598306092542738M, 50340991670837829630733660457M, 28910973119991200164092401367M, 50323158810991832396098070229M, 52414080967812762904973884011M, 52836800429546975705412285929M, 75971341946353016808913594775M, 54107988951112108751504891428M, 23875792211356679989254777364M, 26436004261411981641454114154M, 47460192405019022231898249639M, 52446638114096207455089683116M, 74689578046324202366055843304M, 23902695263491844496948022808M, 34128460711275439826764924200M, 34147809207135683504639466150M, 404248125950292647M };
 }
